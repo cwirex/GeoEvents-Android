01c81b67ebf9b9928c9e23e47b2044ecba858e0b
Jakub Niebrzydowski <kuba.max.niebrzydowski@gmail.com>
2023-06-11 23:34:15 +0200

    AddFriendActivity added

--- app/src/main/java/com/example/tea/menu/FriendsMenuFragment.kt
+++ app/src/main/java/com/example/tea/menu/FriendsMenuFragment.kt
@@ -1,10 +1,14 @@
 package com.example.tea.menu
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.example.tea.AddFriendActivity
 import com.example.tea.R
 
 class FriendsMenuFragment : Fragment() {
@@ -14,7 +18,14 @@ class FriendsMenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends_menu, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_friends_menu, container, false)
+        val addFriendMenuButton = view.findViewById<Button>(R.id.addFriendMenuButton)
 
+        addFriendMenuButton.setOnClickListener {
+            val intent = Intent(activity, AddFriendActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
+    }
 }
