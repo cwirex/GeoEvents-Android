f78dc2f8b43a3144eddee5abc54409796ba9f132
cwirex <ocwiejamateusz@gmail.com>
2023-06-13 14:06:52 +0200

    Map add onClick

--- app/src/main/java/com/example/tea/menu/MapFragment.kt
+++ app/src/main/java/com/example/tea/menu/MapFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tea.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment() {
 
@@ -13,8 +16,23 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+
+        supportMapFragment.getMapAsync { googleMap ->
+            googleMap.setOnMapClickListener { latLng ->
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title("${latLng.latitude} : ${latLng.longitude}")
+                googleMap.clear()
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                googleMap.addMarker(markerOptions)
+            }
+        }
+
+        return view
     }
 
 }
