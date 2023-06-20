01ef3feff6113543cc67e392a089177955cbcc01
Jakub Niebrzydowski <kuba.max.niebrzydowski@gmail.com>
2023-06-20 18:34:38 +0200

    event list sample

--- app/src/main/java/com/example/tea/EventListActivity.kt
+++ app/src/main/java/com/example/tea/EventListActivity.kt
@@ -2,27 +2,36 @@ package com.example.tea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.example.tea.adapters.EventAdapter
 import com.example.tea.adapters.FriendAdapter
 import com.example.tea.databinding.ActivityEventListBinding
 import com.example.tea.databinding.ActivityFriendsBinding
+import com.example.tea.map.Marker
+import com.example.tea.menu.EventMenu
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class EventListActivity : AppCompatActivity() {
     private lateinit var eventAdapter: EventAdapter
-    private lateinit var binding: ActivityEventListBinding
-    private lateinit var auth: FirebaseAuth
-
+    private lateinit var eventRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_list)
+        eventRecyclerView = findViewById(R.id.rv_event_items)
+        eventRecyclerView.layoutManager = LinearLayoutManager(this)
+        eventRecyclerView.setHasFixedSize(true)
+        eventRecyclerView.adapter = EventAdapter(getEventList())
+    }
 
-        auth = Firebase.auth
-        binding = ActivityEventListBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+    private fun getEventList(): ArrayList<EventMenu> {
+        return arrayListOf(
+            EventMenu("jakis Event", Marker(0.0,0.0)),
+            EventMenu("jakis Event", Marker(0.0,0.0))
+        )
     }
 }
